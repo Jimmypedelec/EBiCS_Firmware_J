7686a33cec20e75c59116c54a17f4b5d5719df9a
Jimmypedelec <fv@chipper.de>
2025-03-08 19:34:40 +0100

    Gear ratio auf 22 statt 11

--- Inc/config.h
+++ Inc/config.h
@@ -79,7 +79,7 @@
 
 //-------------------------------Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
-#define GEAR_RATIO 11 //dummy for testing
+#define GEAR_RATIO 22 //dummy for testing 11
 #define SPEEDLIMIT 25
 #define PULSES_PER_REVOLUTION 1
 #define REVERSE -1
