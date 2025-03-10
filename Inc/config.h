29193e79451a37778b0275868e393c79f380fef0
Jimmypedelec <fv@chipper.de>
2025-03-10 13:02:48 +0100

    Update config.h debug2

--- Inc/config.h
+++ Inc/config.h
@@ -63,7 +63,7 @@
 
 //------------------------------ Ride mode setting
 #define NUMBER_OF_PAS_MAGNETS 12
-#define PAS_TIMEOUT 8000
+#define PAS_TIMEOUT 5000
 #define RAMP_END 1600
 //#define DIRDET
 #define FRAC_HIGH 30
@@ -73,7 +73,7 @@
 //#define TQONAD1
 
 //------------------------------Throttle settings
-#define THROTTLE_OFFSET 900
+#define THROTTLE_OFFSET 1500
 #define THROTTLE_MAX 2600
 #define THROTTLE_OVERRIDE
 
