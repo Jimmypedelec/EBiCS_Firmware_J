7fceb09ccbc0f7db0549b1984770765bab243b29
Jimmypedelec <fv@chipper.de>
2025-03-10 12:04:30 +0100

    Update config.h debug

--- Inc/config.h
+++ Inc/config.h
@@ -81,8 +81,8 @@
 #define WHEEL_CIRCUMFERENCE 2200
 #define GEAR_RATIO 22 //dummy for testing 11
 #define SPEEDLIMIT 25
-#define PULSES_PER_REVOLUTION 1
-#define REVERSE -1
+#define PULSES_PER_REVOLUTION 6
+#define REVERSE 1
 #define SPEEDFILTER 1
 #define SPEEDSOURCE INTERNAL
 
@@ -100,10 +100,10 @@
 
 //----------------------------- Display setting
 
-#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
+#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG //DISPLAY_TYPE_KINGMETER_901U
 
 //------------------------------Autodetect setting
-#define AUTODETECT 0
+#define AUTODETECT 1
 
 
 
