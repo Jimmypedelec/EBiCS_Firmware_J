a2b0ebb8531f45c594feeba29d4e8e02457f562e
Jimmypedelec <fv@chipper.de>
2025-06-16 14:14:10 +0200

    Update config.h
    gearratio

--- Inc/config.h
+++ Inc/config.h
@@ -79,7 +79,7 @@
 
 //-------------------------------Speed settings
 #define WHEEL_CIRCUMFERENCE 2075 //2200 originalwert
-#define GEAR_RATIO 80 //dummy for testing 11 bei 80 2 abweichung
+#define GEAR_RATIO 85 //dummy for testing 11 bei 80 2 abweichung bei2200
 #define SPEEDLIMIT 25
 #define PULSES_PER_REVOLUTION 1
 #define REVERSE -1
